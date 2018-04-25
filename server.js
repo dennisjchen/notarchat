c6a8d944bb69d4824bc752746e51054f2a8fdfb4
Dennis Chen <dennis@notarize.com>
2018-04-25 10:53:16 -0400

    add socketio and rxjs

--- server.js
+++ server.js
@@ -1,22 +1,73 @@
+var http = require("http");
 var express = require('express')
 var path = require('path')
 var bodyParser = require('body-parser');
+var immutable = require("immutable");
+var rxjs = require("rxjs");
 
 const port = 3000;
+var usersMap = immutable.Map({});
 const app = express();
 
 app.use(express.static(__dirname + '/dist' ));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, function (error) {
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './dist/index.html'));
+});
+
+app.post('/message', function(req, res) {
+  io.emit('message', req.body);
+});
+
+var server = http.createServer(app);
+var io = require("socket.io").listen(server);
+
+var connect = rxjs.Observable.create(function(observer) {
+  io.on("connection", function(socket) {
+    console.log('Client connected', socket.id);
+    socket.emit('socketId', {'socketId': socket.id });
+    socket.on('client connect', function(data) {
+        observer.onNext({'socket': socket, 'data': data, 'event': 'client connect'});
+    });
+  });
+  return function() {
+    io.close();
+  }
+});
+
+var disconnect = rxjs.Observable.create(function(observer) {
+  io.on("connection", function(socket) {
+    io.on("disconnect", function(data){
+      observer.onNext({'socketId': socket.id, 'event': 'client disconnect'});
+    });
+  });
+  return function() {
+    io.close();
+  }
+});
+
+var observerConnect = connect
+.subscribe(function(obj) {
+    var socketId = obj.data.socketId;
+    usersMap = usersMap.set(socketId, obj.data);
+    //console.log(usersMap);
+    io.emit('all users', usersMap.toArray());
+});
+
+var observerDisconnect = disconnect
+.subscribe(function(obj) {
+    var socketId = obj.socketId;
+    var user = usersMap.get(socketId);
+    usersMap = usersMap.delete(obj.socketId);
+    io.emit('all users', usersMap.toArray());
+});
+
+server.listen(port, function (error) {
   if(error) {
       console.log(error);
   } else {
     console.log("Running on port", port);
   }
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './dist/index.html'));
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,54 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { isEmpty } from "lodash";
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      title: ""
+      name: "",
+      joinedChat: false,
+      messageList: []
     };
+    this.joinChat = this.joinChat.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  joinChat() {
+    this.setState({ joinedChat: true });
+  }
+
+  handleChange(event) {
+    console.log("meow", event);
+    this.setState({ name: event.target.value });
+  }
+
   render() {
+    console.log("currentState:", this.state);
     return (
-      <div>MEOW</div>
+      <div>
+        <h1>Notar-chat</h1>
+        <div>Your name is: {this.state.name}</div>
+        {
+          (this.state.joinedChat && !isEmpty(this.state.name)) ?
+            <div>
+              Messages
+              <div>
+                {
+                  this.state.messageList.map(function(message) {
+                    return <div>{message}</div>
+                  })
+                }
+              </div>
+            </div> :
+            <div>
+              <label>
+                Choose your display name:
+                <input type="text" value={this.state.name} onChange={this.handleChange} placeholder="name" />
+              </label>
+              <button onClick={this.joinChat}>join chat</button>
+            </div>
+        }
+      </div>
     );
   }
 }
