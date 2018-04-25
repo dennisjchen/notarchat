9a23d731bdd30119d858753671fbc2675997ea1c
Dennis Chen <dennis@notarize.com>
2018-04-25 00:30:21 -0400

    small fixes to add bodyparser

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express')
 var path = require('path')
+var bodyParser = require('body-parser');
 
 const port = 3000;
 const app = express();
 
 app.use(express.static(__dirname + '/dist' ));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, function (error) {
   if(error) {
