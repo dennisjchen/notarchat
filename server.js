743b89de9d2f756cfd9735a66bd7fd1690a12d1a
Dennis Chen <dennis@notarize.com>
2018-08-15 00:45:30 -0400

    ??

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ var usersMap = immutable.Map({});
 const app = express();
 
 app.use(express.static(__dirname + '/dist' ));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 
+// Broadcasts a message to all listeners with the message from a user
 app.post('/message', function(req, res) {
   io.emit('message', req.body);
 });
@@ -24,15 +25,19 @@ app.post('/message', function(req, res) {
 var server = http.createServer(app);
 var io = require("socket.io").listen(server);
 
+// Creates an observable that looks for a client connetion and then gives the client an ID for it to use
 var connect = rxjs.Observable.create(function(observer) {
   io.on("connection", function(socket) {
-    console.log('Client connected', socket.id);
     socket.emit('socketId', {'socketId': socket.id });
+    // Executes when there is a successful client connect event
     socket.on('client connect', function(data) {
-        observer.onNext({'socket': socket, 'data': data, 'event': 'client connect'});
+        console.log('Client connected', socket.id);
+        observer.next({'socket': socket, 'data': data, 'event': 'client connect'});
     });
   });
-  return function() {
+
+  // Custom unsubscribe function that will close the socket
+  return function unsubscribe() {
     io.close();
   }
 });
@@ -40,10 +45,11 @@ var connect = rxjs.Observable.create(function(observer) {
 var disconnect = rxjs.Observable.create(function(observer) {
   io.on("connection", function(socket) {
     io.on("disconnect", function(data){
-      observer.onNext({'socketId': socket.id, 'event': 'client disconnect'});
+      console.log('Client disconnected', socket.id);
+      observer.next({'socketId': socket.id, 'event': 'client disconnect'});
     });
   });
-  return function() {
+  return function unsubscribe() {
     io.close();
   }
 });
@@ -52,8 +58,8 @@ var observerConnect = connect
 .subscribe(function(obj) {
     var socketId = obj.data.socketId;
     usersMap = usersMap.set(socketId, obj.data);
-    //console.log(usersMap);
-    io.emit('all users', usersMap.toArray());
+    console.log(usersMap);
+    io.emit('users', usersMap.toArray());
 });
 
 var observerDisconnect = disconnect
@@ -61,7 +67,7 @@ var observerDisconnect = disconnect
     var socketId = obj.socketId;
     var user = usersMap.get(socketId);
     usersMap = usersMap.delete(obj.socketId);
-    io.emit('all users', usersMap.toArray());
+    io.emit('users', usersMap.toArray());
 });
 
 server.listen(port, function (error) {
--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,100 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { isEmpty } from "lodash";
+import openSocket from 'socket.io-client';
+import { Observable } from "rxjs";
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       name: "",
+      text: "",
       joinedChat: false,
-      messageList: []
+      messageList: [],
+      numUsers: 0
     };
     this.joinChat = this.joinChat.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
   }
 
   joinChat() {
+    // Opens connection to socket
+    const socket = openSocket("http://localhost:3000");
+
+    // Creates an observable that executes data from socket when it gets a socketId event
+    var socketConnectObservable = Observable.create(observer => {
+      socket.on("socketId", data => { observer.next(data); });
+    });
+
+    socketConnectObservable.subscribe(data => {
+      socket.emit("client connect", {
+        socketId: data.socketId
+      });
+    });
+
+    var socketUsersObservable = Observable.create(observer => {
+      socket.on("users", data => { observer.next(data) });
+    });
+
+    socketUsersObservable.subscribe(data => {
+      this.setState({ numUsers: data.length });
+    });
+
+    var socketMessagesObservable = Observable.create(observer => {
+      socket.on("message", data => { observer.next(data) });
+    });
+
+    socketMessagesObservable.subscribe(data => {
+      this.setState(prevState => {
+        const newMessageList = prevState.messageList.concat(data.text);
+        return { messageList: newMessageList };
+      });
+    });
+
     this.setState({ joinedChat: true });
   }
 
   handleChange(event) {
-    console.log("meow", event);
     this.setState({ name: event.target.value });
   }
 
+  handleTextChange(event) {
+    this.setState({ text: event.target.value });
+  }
+
+  sendMessage() {
+    fetch("http://localhost:3000/message", {
+      method: "POST",
+      body: JSON.stringify({ text: this.state.text }),
+      headers: {
+        "content-type": "application/json"
+      }
+    }).then(function(res) {
+      this.setState({ text: "" });
+    }).catch(function() {
+      console.log("sendmessage failed");
+    });
+  }
+
   render() {
-    console.log("currentState:", this.state);
     return (
       <div>
         <h1>Notar-chat</h1>
+        <div>Number of Users in chat: {this.state.numUsers}</div>
         <div>Your name is: {this.state.name}</div>
         {
           (this.state.joinedChat && !isEmpty(this.state.name)) ?
             <div>
+              <div>
+                <label>
+                  Talk in chat:
+                  <input type="text" value={this.state.text} onChange={this.handleTextChange} placeholder="text" />
+                </label>
+                <button onClick={this.sendMessage}>send</button>
+              </div>
               Messages
               <div>
                 {
